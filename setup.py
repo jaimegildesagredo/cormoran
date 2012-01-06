67255047be2c8b76a9ef27322407fd0f1e389843
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2012-01-06 12:40:02 +0100

    Created the initial project structure.

---
+++ setup.py
@@ -0,0 +1,49 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Cormoran is a fast and lightweight persistence framework.
+# Copyright (C) 2012 Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+""""""
+
+from setuptools import setup, find_packages
+
+import cormoran
+
+setup(
+    name='cormoran',
+    version=cormoran.__version__,
+    description='Cormoran is a fast and lightweight persistence framework',
+    long_description=cormoran.__doc__,
+    author='Jaime Gil de Sagredo Luna',
+    author_email='jaimegildesagredo@gmail.com',
+    url='https://github.com/jaimegildesagredo/cormoran',
+    download_url='https://github.com/jaimegildesagredo/cormoran/downloads',
+    license='GPLv3',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    test_suite='tests',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    )
+
