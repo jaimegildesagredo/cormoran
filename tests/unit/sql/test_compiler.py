278ed5ea4cddaf475d8597b72508626ae6677eb6
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2012-05-15 15:52:47 +0200

    Moved all SQLCompiler tests to the same test case

--- tests/unit/sql/test_compiler.py
+++ tests/unit/sql/test_compiler.py
@@ -15,25 +15,25 @@ class Group(Persistent):
     name = StringField(name='groupname')
 
 
-class TestSelect(unittest.TestCase):
-    def test_compile_returns_compiled_sql_and_params(self):
+class TestSQLCompiler(unittest.TestCase):
+    def test_select_returns_sql_and_params(self):
         compiled, params = self.compiler.select(User)
 
         assert_that(compiled, is_('SELECT _id, name FROM User'))
         assert_that(params, has_length(0))
 
-    def test_compile_with_different_field_names_use_alias(self):
+    def test_select_with_different_field_names_uses_alias(self):
         compiled, params = self.compiler.select(Group)
 
         assert_that(compiled, is_('SELECT _id, groupname AS name FROM Group'))
 
-    def test_compile_with_filter_adds_where_clause(self):
+    def test_select_with_filter_uses_where_clause(self):
         compiled, params = self.compiler.select(User, filters={'name': u'Mike'})
 
         assert_that(compiled, is_('SELECT _id, name FROM User WHERE name=?'))
         assert_that(params, contains(u'Mike'))
 
-    def test_compile_with_filters_adds_where_clause(self):
+    def test_select_with_filters_uses_where_clause(self):
         compiled, params = self.compiler.select(User,
             filters={'name': u'Mike', '_id': 1})
 
@@ -41,58 +41,41 @@ class TestSelect(unittest.TestCase):
             'SELECT _id, name FROM User WHERE _id=? AND name=?'))
         assert_that(params, contains(1, u'Mike'))
 
-    def test_compile_with_limit_stop_adds_limit_clause(self):
+    def test_select_with_limit_stop_uses_limit_clause(self):
         compiled, params = self.compiler.select(User, limit=slice(1))
 
         assert_that(compiled, is_('SELECT _id, name FROM User LIMIT 1'))
 
-    def test_compile_with_limit_start_and_stop_adds_offset_clause(self):
+    def test_select_with_limit_start_and_stop_uses_offset_clause(self):
         compiled, params = self.compiler.select(User, limit=slice(1, 1))
 
         assert_that(compiled, is_('SELECT _id, name FROM User LIMIT 1 OFFSET 1'))
 
-    def test_compile_with_empty_slice_limit_doesnt_adds_limit_clause(self):
+    def test_select_with_empty_slice_limit_doesnt_uses_limit_clause(self):
         compiled, params = self.compiler.select(User, limit=slice(None))
 
         assert_that(compiled, is_('SELECT _id, name FROM User'))
 
-    def setUp(self):
-        self.compiler = SQLCompiler()
-
-
-class TestUpdate(unittest.TestCase):
-    def test_compile_returns_compiled_sql_and_params(self):
+    def test_update_returns_sql_and_params(self):
         compiled, params = self.compiler.update(self.user)
 
         assert_that(compiled, is_('UPDATE User SET _id=?, name=? WHERE _id=?'))
         assert_that(params, contains(1, u'Bob', 1))
 
-    def setUp(self):
-        self.user = User(_id=1, name=u'Bob')
-        self.compiler = SQLCompiler()
-
-
-class TestDelete(unittest.TestCase):
-    def test_compile_returns_compiled_sql_and_params(self):
+    def test_delete_returns_sql_and_params(self):
         compiled, params = self.compiler.delete(self.user)
 
         assert_that(compiled, is_('DELETE FROM User WHERE _id=?'))
         assert_that(params, contains(1))
 
-    def setUp(self):
-        self.user = User(_id=1, name=u'Bob')
-        self.compiler = SQLCompiler()
-
-
-class TestInsert(unittest.TestCase):
-    def test_compile_returns_compiled_sql_and_params(self):
+    def test_insert_returns_sql_and_params(self):
         compiled, params = self.compiler.insert(self.user)
 
         assert_that(compiled, is_(
             'INSERT INTO User (_id, name) VALUES (?, ?)'))
         assert_that(params, contains(1, u'Bob'))
 
-    def test_compile_with_different_field_names(self):
+    def test_insert_with_different_field_names(self):
         compiled, params = self.compiler.insert(self.group)
 
         assert_that(compiled, is_(
