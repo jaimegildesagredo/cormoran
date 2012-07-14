d350df214aac8eefdd1ae51e0f2fc2fdbc0d7229
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2012-07-14 17:00:12 +0200

    The MySQL backend reconnects to the server when beings idle

--- cormoran/backends/mysql.py
+++ cormoran/backends/mysql.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import time
+
 from cormoran.persistence import Persistence as Persistence_
 from cormoran.sql.compiler import SQLCompiler
 
@@ -8,13 +10,44 @@ import MySQLdb as dbapi2
 
 class Persistence(Persistence_):
     def __init__(self, uri):
-        self._connection = dbapi2.connect(**uri)
+        self._connection = None
         self._transaction = False
+        self._last_use_time = time.time()
 
+        self.uri = uri
         self.compiler = SQLCompiler(placeholder='%s')
+        self.connection_recycle = uri.options.get('connection_recycle', 7200)
+
+        self.reconnect()
+
+    def __del__(self):
+        self.close()
 
     def _cursor(self):
+        self._ensure_connected()
         return self._connection.cursor(dbapi2.cursors.DictCursor)
+
+    def _ensure_connected(self):
+        """Based on the Connection class from tornado.database module.
+
+            http://www.tornadoweb.org/documentation/_modules/tornado/database.html#Connection
+
+        """
+
+        if self._connection is None or \
+            time.time() - self._last_use_time > self.connection_recycle:
+
+            self.reconnect()
+        self._last_use_time = time.time()
+
+    def reconnect(self):
+        self.close()
+        self._connection = dbapi2.connect(**self.uri)
+
+    def close(self):
+        if self._connection is not None:
+            self._connection.close()
+            self._connection = None
 
     def begin_transaction(self):
         if not self._transaction:
